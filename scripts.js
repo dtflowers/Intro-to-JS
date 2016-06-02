76bd7355699756e941f7c92f319b9bfbbb2574c7
Douglass Flowers <dtflowers@gmail.com>
2016-06-02 10:50:22 -0500

    expermient with global variables, functions, and scope

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
-function sayHello() {
-    var name = 'Douglass';
-    alert('Hello ' + name)
+var age = 31;
+
+function getAgeInDogYears(humanAge) {
+    var dogAge = humanAge * 7;
+    return dogAge;
 }
 
-sayHello();
+
+function sayHello(name) {
+    age = 23;
+    alert('Hello, ' + name + '. Your age is ' + getAgeInDogYears(age) + '.');
+    age = getAgeInDogYears(age);
+}
+
+sayHello('Tyler');
+console.log(age);
