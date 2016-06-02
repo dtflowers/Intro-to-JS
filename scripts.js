41b6a6b3ff046ea55dca75613e401f53678441a7
Douglass Flowers <dtflowers@gmail.com>
2016-06-02 11:02:43 -0500

    experiment with arrays and objects

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
 var age = 31;
 
+var david = {
+    name: 'David',
+    email: 'david@somedomain.com'
+};
+var tyler = {
+    name: 'Tyler',
+    email: 'tyler@somedomain.com'
+};
+var haley = {
+    name: 'Haley',
+    email: 'haley@somedomain.com'
+};
+
+var staff = [tyler, haley, haley];
+
+console.log(staff);
+console.log(staff[2]);
+
 function getAgeInDogYears(humanAge) {
     var dogAge = humanAge * 7;
     return dogAge;
